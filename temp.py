6d369070c8656a8d843adb9dba72cae9da234039
REDummy <215314181@student.usd.ac.id>
2024-02-19 22:15:08 +0700

    Catching up commits

--- temp.py
+++ temp.py
@@ -5,40 +5,10 @@ from sklearn.cluster import KMeans, DBSCAN, AgglomerativeClustering
 import seaborn as sns
 from ucimlrepo import fetch_ucirepo
 
-# Load the data
-heart_failure_clinical_records = fetch_ucirepo(id=519)
-target = heart_failure_clinical_records.data.targets
-data = heart_failure_clinical_records.data.features
+# fetch dataset
+breast_cancer_wisconsin_diagnostic = fetch_ucirepo(id=17)
 
-# Perform k-means clustering
-kmeans = KMeans(n_clusters=3)
-kmeans.fit(data)
-kmeans_labels = kmeans.labels_
+temp = breast_cancer_wisconsin_diagnostic.data.original
 
-# Perform DBSCAN clustering
-dbscan = DBSCAN(eps=0.5, min_samples=10)
-dbscan.fit(data)
-dbscan_labels = dbscan.labels_
+temp.to_csv('breast_cancer.csv', sep=',', index=False)
 
-# Perform hierarchical clustering
-ward = AgglomerativeClustering(n_clusters=3, affinity='euclidean', linkage='ward')
-ward.fit(data)
-ward_labels = ward.labels_
-
-# Plot the data using pairplot
-sns.set(style="ticks")
-sns.pairplot(data, hue=kmeans_labels)
-plt.suptitle("Pair Plot with KMeans Clusters", y=1.02)
-plt.show()
-
-# Plot the data using pairplot
-sns.set(style="ticks")
-sns.pairplot(data, hue=dbscan_labels)
-plt.suptitle("Pair Plot with DBSCAN Clusters", y=1.02)
-plt.show()
-
-# Plot the data using pairplot
-sns.set(style="ticks")
-sns.pairplot(data, hue=ward_labels)
-plt.suptitle("Pair Plot with Hierarchical Clusters", y=1.02)
-plt.show()
